e9d252dea7a536f0c9a7191b68338cc1426064bb
Amanuel Bekele <159442378+GemmechuBekele@users.noreply.github.com>
2026-03-13 15:16:47 +0300

    Add solution for Range Sum Query 2D - Immutable

--- leetcode/0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ leetcode/0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -1,30 +1,30 @@
 class NumMatrix:
 
-    def __init__(self, matrix: List[List[int]]):
-        if not matrix:
-            return
+    def __init__(self, matrix):
         m, n = len(matrix), len(matrix[0])
-        self.prefix = [[0]*(n+1) for _ in range(m+1)]
+        self.pref = [[0]*(n+1) for _ in range(m+1)]
 
-        for r in range(1, m + 1):
-            for c in range(1, n + 1):
-                self.prefix[r][c] = (
+        for r in range(1, m+1):
+            for c in range(1, n+1):
+                self.pref[r][c] = (
                     matrix[r-1][c-1]
-                    + self.prefix[r-1][c]
-                    + self.prefix[r][c-1]
-                    - self.prefix[r-1][c-1]
+                    + self.pref[r-1][c]
+                    + self.pref[r][c-1]
+                    - self.pref[r-1][c-1]
                 )
-        
 
-    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+    def sumRegion(self, row1, col1, row2, col2):
+        row1 += 1
+        col1 += 1
+        row2 += 1
+        col2 += 1
+
         return (
-            self.prefix[row2 + 1][col2 + 1]
-            - self.prefix[row1][col2 + 1]
-            - self.prefix[row2 + 1][col1]
-            + self.prefix[row1][col1]
+            self.pref[row2][col2]
+            - self.pref[row1-1][col2]
+            - self.pref[row2][col1-1]
+            + self.pref[row1-1][col1-1]
         )
-        
-
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
