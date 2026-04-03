b9e53929e107cdd355a4d50c0c2328fce7ddabfc
Amanuel Bekele <159442378+GemmechuBekele@users.noreply.github.com>
2026-04-03 17:52:51 +0300

    Add solution for Heaters

---
+++ leetcode/0475-heaters/0475-heaters.py
@@ -0,0 +1,30 @@
+class Solution:
+    import bisect
+
+    def possible(self, r, heaters, houses):
+        for house in houses:
+            # Find the position where this house would fit in heaters
+            idx = bisect.bisect_left(heaters, house)
+            
+            # distance to left heater
+            left = abs(house - heaters[idx-1]) if idx > 0 else float('inf')
+            # distance to right heater
+            right = abs(heaters[idx] - house) if idx < len(heaters) else float('inf')
+            
+            if min(left, right) > r:
+                return False
+        return True
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        heaters.sort()
+        houses.sort()
+        l, r = 0, 10**9
+        ans = float("inf")
+        while l <= r:
+            mid = l + (r-l)//2
+            if self.possible(mid, heaters, houses):
+                ans = mid
+                r = mid-1
+            else:
+                l = mid + 1
+        return ans
+        
