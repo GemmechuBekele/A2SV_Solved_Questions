979c6e8105ff3fd846f03d9454aabde4dcef879c
Amanuel Bekele <159442378+GemmechuBekele@users.noreply.github.com>
2026-04-09 18:49:47 +0300

    Add solution for Sort List

--- leetcode/0148-sort-list/0148-sort-list.py
+++ leetcode/0148-sort-list/0148-sort-list.py
@@ -3,48 +3,90 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# class Solution:
+#     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         if not head or not head.next:
+#             return head
+
+#         # find the middle of the list
+#         step1_move = head
+#         step2_move = head.next
+
+#         while step2_move and step2_move.next:
+#             step1_move = step1_move.next
+#             step2_move = step2_move.next.next
+
+#         # Split list (Cut the list into two halves)
+#         mid = step1_move.next
+#         step1_move.next = None
+
+#         # Sort both halves
+#         left = self.sortList(head)
+#         right = self.sortList(mid)
+
+#         #  Merge sorted halves
+#         return self.merge(left, right)
+
+#     def merge(self, left, right):
+#         fake_start = ListNode(0)
+#         tail = fake_start
+
+#         while left and right:
+#             if left.val < right.val:
+#                 tail.next = left
+#                 left= left.next
+#             else:
+#                 tail.next = right
+#                 right = right.next
+#             tail = tail.next
+
+#         # Attach remaining nodes
+#         if left:
+#             tail.next = left
+#         if right:
+#             tail.next = right
+
+#         return fake_start.next
+
+
 class Solution:
-    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def sortList(self, head: ListNode) -> ListNode:
         if not head or not head.next:
             return head
 
-        # Find middle
-        slow = head
-        fast = head.next
+        mid = self.findMid(head)
+        right = mid.next
+        mid.next = None         
 
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
+        left_sorted  = self.sortList(head)
+        right_sorted = self.sortList(right)
 
-        # Split list
-        mid = slow.next
-        slow.next = None
+        return self.merge(left_sorted, right_sorted)
 
-        # Sort both halves
-        left = self.sortList(head)
-        right = self.sortList(mid)
+    def findMid(self, head):
+        one_step_move = head
+        two_step_move = head.next       
 
-        #  Merge sorted halves
-        return self.merge(left, right)
+        while two_step_move and two_step_move.next:
+            one_step_move = one_step_move.next
+            two_step_move = two_step_move.next.next
 
-    def merge(self, l1, l2):
-        dummy = ListNode(0)
-        tail = dummy
+        return one_step_move            
 
-        while l1 and l2:
-            if l1.val < l2.val:
-                tail.next = l1
-                l1 = l1.next
+    def merge(self, left, right):
+        fake_start = ListNode(0)     
+        current = fake_start
+
+        while left and right:
+            if left.val <= right.val:
+                current.next = left
+                left = left.next
             else:
-                tail.next = l2
-                l2 = l2.next
-            tail = tail.next
+                current.next = right
+                right = right.next
+            current = current.next
 
-        # Attach remaining nodes
-        if l1:
-            tail.next = l1
-        if l2:
-            tail.next = l2
+        current.next = left if left else right
 
-        return dummy.next
-        
+        return fake_start.next  
+      
