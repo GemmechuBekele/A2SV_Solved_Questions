cd6b0a2f5f3a790d813cc35856518022653905b8
Amanuel Bekele <159442378+GemmechuBekele@users.noreply.github.com>
2026-04-15 19:25:01 +0300

    Add solution for First Missing Positive

---
+++ leetcode/0041-first-missing-positive/0041-first-missing-positive.py
@@ -0,0 +1,20 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        arr = self.cycle_sort(nums)
+        m = len(arr)
+        for i in range(m):
+            if nums[i] != i+1:
+                return i+1
+        return m+1
+    def cycle_sort(self, nums):
+        n = len(nums)
+        i = 0
+        while i < n:
+            if 1 <= nums[i] and nums[i] <= n:
+                pos = nums[i] - 1
+                if nums[i] != nums[pos]:
+                    nums[i], nums[pos] = nums[pos], nums[i]
+                    continue
+            i += 1
+        return nums
+        
