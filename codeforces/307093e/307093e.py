280229a2bca3196194539702c94fc2f266645b89
Amanuel Bekele <159442378+GemmechuBekele@users.noreply.github.com>
2026-04-17 19:55:59 +0300

    Add solution for 307093e

--- codeforces/307093e/307093e.py
+++ codeforces/307093e/307093e.py
@@ -1,24 +1,25 @@
 from collections import defaultdict
-
 n, k = map(int, input().split())
 a = list(map(int, input().split()))
 
 freq = defaultdict(int)
-left = 0
-ans = 0
-unique = 0
+distinct = 0
+l = 0
+r = 0
+count = 0
+while r < n:
+    if freq[a[r]] == 0:
+        distinct += 1
+    freq[a[r]] += 1
 
-for right in range(n):
-    freq[a[right]] += 1
-    if freq[a[right]] == 1:
-        unique += 1
+    while distinct > k:
+        freq[a[l]] -= 1
+        if freq[a[l]] == 0:
+            distinct -= 1
+        l += 1
 
-    while unique > k:
-        freq[a[left]] -= 1
-        if freq[a[left]] == 0:
-            unique -= 1
-        left += 1
+    count += (r - l + 1)
 
-    ans += right - left + 1
+    r += 1
 
-print(ans)
+print(count)
