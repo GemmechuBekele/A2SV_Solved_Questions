556c027d81ba26be8e663a01ff346908f77591d4
Amanuel Bekele <159442378+GemmechuBekele@users.noreply.github.com>
2026-04-22 19:46:29 +0300

    Add solution for Find if Path Exists in Graph

--- leetcode/1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ leetcode/1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,15 +1,40 @@
-from collections import defaultdict
-from typing import List
+# class Solution:
+#     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+#         graph = defaultdict(list)
+
+#         for u, v in edges:
+#             graph[u].append(v)
+#             graph[v].append(u)
+
+#         visited = set()
+#         stack = [source]
+
+#         while stack:
+#             node = stack.pop()
+
+#             if node == destination:
+#                 return True
+
+#             if node not in visited:
+#                 visited.add(node)
+
+#                 for neighbor in graph[node]:
+#                     if neighbor not in visited:
+#                         stack.append(neighbor)
+
+#         return False
+
 
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        graph = defaultdict(list)
-
-        for u, v in edges:
-            graph[u].append(v)
-            graph[v].append(u)
 
         visited = set()
+        graph = defaultdict(list)
+
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+
         stack = [source]
 
         while stack:
@@ -21,8 +46,8 @@ class Solution:
             if node not in visited:
                 visited.add(node)
 
-                for neighbor in graph[node]:
-                    if neighbor not in visited:
-                        stack.append(neighbor)
+                for nei in graph[node]:
+                    if nei not in visited:
+                        stack.append(nei)
 
         return False
